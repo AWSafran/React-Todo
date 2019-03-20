4930ed89773fca1dd96cb2eb64f49687b7c60931
AWSafran <andrewsafran@gmail.com>
2019-03-20 15:28:11 -0400

    began building app methods and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
+const toDo = [
+  {
+    task: "Learn React",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "Struggle",
+    id: Date.now(),
+    completed: false
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+
+    this.state = {
+      toDoList: toDo,
+      task: ''
+    }
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  retrieveInput = event =>{
+    console.log("Retrieving Input ", event.target.value);
+    this.setState({
+      task: event.target.value
+    })
+  }
+
+  updateList = event =>{
+    event.preventDefault();
+    const newItem = {
+      task: this.state.task,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+      toDoList: [...this.state.toDoList, newItem]
+    });
+  }
+
   render() {
     return (
       <div>
